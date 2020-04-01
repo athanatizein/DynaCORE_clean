498f9efee79e2706007e0a38f6686e807caa32a0
Lara Puhlmann <puhlmann@cbs.mpg.de>
2020-04-01 15:21:09 +0200

    created file

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Set up pipeline to clean survey data from DynaCORE
 
 contributers: Kenneth Yuen, Lara Puhlmann, Matthias Zerban
+
+For the variable manual please see XXX 
---
+++ cleaning_main.R
@@ -0,0 +1,7 @@
+# pipeline template to clean and structure data from the DynaCORE project for analysis
+# contributors:
+# Matthias Zerban ()
+# Kenneth Yuen ()
+# Lara Puhlmann (puhlmann@cbs.mpg.de)
+
+# load data
