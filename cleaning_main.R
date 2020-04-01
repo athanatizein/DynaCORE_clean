733577407fb3ab00c1cec91a7395e22a4c7058e9
Lara Puhlmann <puhlmann@cbs.mpg.de>
2020-04-01 21:55:13 +0200

    update

--- cleaning_main.R
+++ cleaning_main.R
@@ -11,19 +11,18 @@ require(dplyr)
 # source
 
 # load data and add column indicating the origin of the data
-data_ger = read.csv(".csv", sep = ",", stringsAsFactors = FALSE)
-data_ger$survey = as.factor("ger")
+data_ger = read.csv("DynaCORE_test_data_ger.csv", sep = ",", stringsAsFactors = FALSE)
+data_ger$survey_country = as.factor("ger")
 
-data_xx = read.csv(".csv", sep = ",", stringsAsFactors = FALSE)
-data_xx$survey = as.factor("xx")
+data_xx = read.csv("DynaCORE_test_data_xx.csv", sep = ",", stringsAsFactors = FALSE)
+data_xx$survey_country = as.factor("xx")
 
 data_xy = read.csv(".csv", sep = ",", stringsAsFactors = FALSE)
-data_xy$survey = as.factor("xy")
-
-data_all = merge(data_ger, data_xx, data_xy)
+data_xy$survey_country = as.factor("xy")
 
 # combine files from multiple languages - this is for later
-
+data_all = merge(data_ger, data_xx, data_xy)
+xx = which(!is.na(data_ger$Respondent.ID))
 # identify NAs, subjects to be excluded entirely..
 
 # compile variables of interest
