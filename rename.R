c3f5211a97675c6b16851c4d6e842b0dd5618896
Lara Puhlmann <puhlmann@cbs.mpg.de>
2020-04-02 21:02:25 +0200

    updated occupation stuff

--- rename.R
+++ rename.R
@@ -141,55 +141,56 @@ rename <- function(df){
 # While it is possible to use grepl to match the strings in multiple choice questions, I was not sure if the text will be different depending on the language of the survey. So I tried a workaround based on position in the survey only - LP
   
 #x <- vector(mode = "list", length = 0)
-for(i in 1:length(df$Respondent.ID)){
-  x <- vector(mode = "list", length = 0)
-  if(nchar(df$X.1[i]) > 0){
-    x[[length(x)+1]] = 1
-  } 
-  if(nchar(df$X.2[i]) > 0){
-    x[[length(x)+1]] = 2
-  } 
-  if(nchar(df$X.3[i]) > 0){
-    x[[length(x)+1]] = 3
-  } 
-  if(nchar(df$X.4[i]) > 0){
-    x[[length(x)+1]] = 4
-  } 
-  if(nchar(df$X.5[i]) > 0){
-    x[[length(x)+1]] = 5
-  } 
-  if(nchar(df$X.6[i]) > 0){
-    x[[length(x)+1]] = 6
+  for(i in 1:length(df$Respondent.ID)){
+    #x <- vector(mode = "any", length = 0)
+    x <- vector()
+    if(nchar(df$X.1[i]) > 0){
+      x[length(x)+1] = 1
+    } 
+    if(nchar(df$X.2[i]) > 0){
+      x[length(x)+1] = 2
+    } 
+    if(nchar(df$X.3[i]) > 0){
+      x[length(x)+1] = 3
+    } 
+    if(nchar(df$X.4[i]) > 0){
+      x[length(x)+1] = 4
+    } 
+    if(nchar(df$X.5[i]) > 0){
+      x[length(x)+1] = 5
+    } 
+    if(nchar(df$X.6[i]) > 0){
+      x[length(x)+1] = 6
+    }
+    if(nchar(df$X.7[i]) > 0){
+      x[length(x)+1] = 6
+    }
+    if(nchar(df$X.8[i]) > 0){
+      x[length(x)+1] = 6
+    }
+    if(nchar(df$X.9[i]) > 0){
+      x[length(x)+1] = 6
+    }
+    if(nchar(df$X.10[i]) > 0){
+      x[length(x)+1] = 6
+    }
+    if(nchar(df$X.11[i]) > 0){
+      x[length(x)+1] = 6
+    }
+    if(nchar(df$X.12[i]) > 0){
+      x[length(x)+1] = 6
+    }
+    if(nchar(df$X.13[i]) > 0){
+      x[length(x)+1] = 6
+    }
+    if(nchar(df$X.14[i]) > 0){
+      x[length(x)+1] = 6
+    }
+    if(nchar(df$X.15[i]) > 0){
+      x[length(x)+1] = df$X.15[i]
+    }
+    df$occupation[i] = list(x)
   }
-  if(nchar(df$X.7[i]) > 0){
-    x[[length(x)+1]] = 6
-  }
-  if(nchar(df$X.8[i]) > 0){
-    x[[length(x)+1]] = 6
-  }
-  if(nchar(df$X.9[i]) > 0){
-    x[[length(x)+1]] = 6
-  }
-  if(nchar(df$X.10[i]) > 0){
-    x[[length(x)+1]] = 6
-  }
-  if(nchar(df$X.11[i]) > 0){
-    x[[length(x)+1]] = 6
-  }
-  if(nchar(df$X.12[i]) > 0){
-    x[[length(x)+1]] = 6
-  }
-  if(nchar(df$X.13[i]) > 0){
-    x[[length(x)+1]] = 6
-  }
-  if(nchar(df$X.14[i]) > 0){
-    x[[length(x)+1]] = 6
-  }
-  if(nchar(df$X.15[i]) > 0){
-    x[[length(x)+1]] = df$X.15[i]
-  }
-  df$occupation[i] = list(x)
-}
  
 
 #  l <- sapply(colnames(df$X.1), function(x) grep("Arts", df$X.1[,x]))
